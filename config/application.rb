d84fdf7ae3865f730bd74b748dc360d035630c28
chetan-redkivi <chetan.tomar@redkivi.com>
2012-06-22 12:53:24 +0530

    last2

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Exp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-		config.assets.initialize_on_precompile = false
+#		config.assets.initialize_on_precompile = false
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
